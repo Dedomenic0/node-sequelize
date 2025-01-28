db11d99f3f17f087899d0fa460295090a2a49fae
Dedomenic0 <eduardo.dedomenico@gmail.com>
2025-01-28 13:27:16 -0300

    finalização

--- src/controllers/PessoaController.js
+++ src/controllers/PessoaController.js
@@ -7,6 +7,18 @@ class PessoaController extends Controller{
     constructor() {
         super(pessoaService);
     }
+
+    async pegaMatriculas (req, res) {
+        const { estudanteid } = req.params;
+
+        try {
+            const listaMatriculas = await pessoaService.pegaMatriculasPorEstudantes( Number ( estudanteid ));
+            return res.status(200).json(listaMatriculas);
+        } catch (erro) {
+            //erro
+        }
+    }
+
 }
 
 module.exports = PessoaController;
--- src/services/PessoaService.js
+++ src/services/PessoaService.js
@@ -4,6 +4,13 @@ class PessoaServices extends Services {
     constructor() {
         super('Pessoa');
     }
+
+    async pegaMatriculasPorEstudantes(id) {
+        const estudante = await super.pegaUmRegistroPorId(id);
+        const listaMatriculas = await estudante.getAulasMatriculadas(id);
+        return listaMatriculas;
+    }
+
 }
 
 module.exports = PessoaServices;
