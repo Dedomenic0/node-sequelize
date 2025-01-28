c95d83274e5743cdcfc000a81cde862c087af523
Dedomenic0 <eduardo.dedomenico@gmail.com>
2025-01-28 12:53:05 -0300

    aualizaando

--- src/services/Services.js
+++ src/services/Services.js
@@ -1,3 +1,4 @@
+const { where } = require('sequelize');
 const dataSource = require('../models');
 
 class Services {
@@ -8,6 +9,31 @@ class Services {
     async pegaTodosOsRegistros() {
         return dataSource[this.model].findAll();
     }
-}
 
+     async pegaUmRegistroPorId(id) {
+    return dataSource[this.model].findByPk(id);
+  }
+
+  async criaRegistro(dadosDoRegistro) {
+    return dataSource[this.model].create(dadosDoRegistro);
+  }
+    
+    async autalizaRegistro(dadosAtualizados, id) {
+        const listaDeRegistrosAtualizados = dataSource[this.model].update(dadosAtualizados, {
+            where: {
+                id: id
+            }
+        });
+        if (listaDeRegistrosAtualizados[0] === 0) {
+            return false;
+        }
+        return true;
+    }
+
+    async excluiRegistro(id) {
+    return dataSource[this.model].destroy({ where: { id: id } });
+  }
+
+}
+    
 module.exports = Services;
