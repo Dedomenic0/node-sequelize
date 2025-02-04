3a542922319c27f55e1e664798c566b488fcb5d9
Dedomenic0 <eduardo.dedomenico@gmail.com>
2025-02-04 00:42:26 -0300

    validacao

--- src/controllers/PessoaController.js
+++ src/controllers/PessoaController.js
@@ -19,6 +19,14 @@ class PessoaController extends Controller{
         }
     }
 
+    async pegaTodasAsPessoas (req, res) {
+        try {
+            const listaTodasAsPessoas = await pessoaService.pegaPessoasEscopoTodos();
+            return res.status(200).json(listaTodasAsPessoas);
+        } catch (erro) {
+            return res.status(500).json({ erro: erro.message });
+        }
+    }
 }
 
 module.exports = PessoaController;
--- src/services/PessoaService.js
+++ src/services/PessoaService.js
@@ -11,6 +11,11 @@ class PessoaServices extends Services {
         return listaMatriculas;
     }
 
+    async pegaPessoasEscopoTodos () {
+        const listaPessoas = await super.pegaRegistroPorEscopo('todosOsRegistros');
+        return listaPessoas;
+    }
+
 }
 
 module.exports = PessoaServices;
